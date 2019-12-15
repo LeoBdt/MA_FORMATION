e6cdeaf73b60f479721703c52ae102001b9d47a6
LeooBdt <toonbunny@gmail.com>
2019-12-15 21:47:53 +0100

    Début

---
+++ COURS/variables.php
@@ -0,0 +1,25 @@
+<?php 
+
+# Une variable est une valeur, cela peut être un chiffre, une lettre ou une chaine de caractères.
+
+# J'enregistre la variable de mon nom et de mon age, j'utilise ensuite echo pour l'afficher.
+
+
+$nom = 'Léo';
+$age = 17;
+
+echo $nom . " " . $age;
+
+# Ici, je vais faire une opération entre deux variablesn je fais une moyenne.
+
+$note1 = 10;
+$note2 = 18;
+
+echo ($note1 + $note2) / 2;
+
+
+# Les bouleins permettent de faire de sauvegarder des variables vraies ou fausses.
+
+$prenom = true;
+$prenom2 = false;
+$prenom3 = null; # Cette variable est nulle.
---
+++ EXERCICES/variables.php
@@ -0,0 +1,13 @@
+<?php 
+
+# INFORMATIONS PERSO : 
+$prenom = 'Ella';
+$nom = 'Wheatley';
+
+# NOTES :
+
+$note1 = 10;
+$note2 = 17;
+
+
+echo 'Bonjour' . $prenom . ' '. $nom . 'vous avez eu ' . ($note1 + $note2 /2) . 'de moyenne.';
