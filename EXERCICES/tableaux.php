8e747a7bc2f3fc37d9d949bc6d87e2b6433db67a
LeooBdt <toonbunny@gmail.com>
2019-12-17 16:47:40 +0100

    Create tableaux.php: exercice
    Voilà, j'ai terminé mon deuxième exercice. Je reviendrais surement dessus plus tard.

---
+++ EXERCICES/tableaux.php
@@ -0,0 +1,32 @@
+<?php
+
+$profils = [
+    [
+        'id' => 19763,
+        'nom' => 'Bidot',
+        'prenom' => 'Léo',
+        'age' => 17,
+        'taille' => 1.78,
+        'ville' => 'Béziers'
+    ],
+    [
+        'id' => 53864,
+        'nom' => 'Affre',
+        'prenom' => 'Jean',
+        'age' => 25,
+        'taille' => 2.47,
+        'ville' =>  'Paris'
+    ],
+    [
+        'id' => 40925,
+        'nom' => 'Batanemo',
+        'prenom' => 'Emilien',
+        'age' => 12,
+        'taille' => 1.54,
+        'ville' => 'Montpellier'
+    ],
+];
+
+print_r($profils);
+
+# Voilà, c'était mon deuxième exercice, il me servira surement plus tard, mais pour l'instant, j'en suis rendu à là.
