d4ee04de39bc13f50d4320f4523383da5ad9ec83
Léo <toonbunny@gmail.com>
2023-03-14 12:19:24 +0100

    typo
    fixed some typos

--- COURS/cours_graphikart/fonctions_utilisateurs.php
+++ COURS/cours_graphikart/fonctions_utilisateurs.php
@@ -3,7 +3,7 @@
 # Une fonction est utile, elle permet d'éviter de se répéter, je vais en créer une.
 
 function bonjour ($nom) {
-    echo "Bonjour $nom";
+    echo "Bonjour $nom et bienvenue sur mon site !";
 }
 
 bonjour ("jean");
